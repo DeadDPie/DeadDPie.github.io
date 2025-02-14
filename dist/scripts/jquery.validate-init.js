4cd0f1fe6b30d6a577ee99aa7b9d5a0c426c307f
Субботина Олеся <elizabethfox434@gmail.com>
2025-02-14 13:06:44 +0700

    add toaster

--- dist/scripts/jquery.validate-init.js
+++ dist/scripts/jquery.validate-init.js
@@ -47,4 +47,48 @@
 			errorElement: "span",
 		});
 	}
+
+	const subscribeForm = $("#js-subscribeForm");
+	if (subscribeForm.length) {
+		const subscribeAction = subscribeForm.attr("action");
+		const subscribeEmail = subscribeForm.find("#js-subscribeEmail");
+
+		subscribeForm.validate({
+			errorElement: "span",
+			submitHandler: function (form, event) {
+				event.preventDefault();
+
+				// $.ajax({
+				//   url: subscribeAction,
+				//   method: "POST",
+				//   data: {
+				//     email: subscribeEmail.val()
+				//   },
+				//   success: function () {
+				//     subscribeEmail.val("");
+				//     subscribeEmail.blur();
+				//     alert("Вы успешно подписались на рассылку новостей");
+				//   },
+				//   error: function () {
+				//     alert("Что-то пошло не так, попробуйте еще раз");
+				//   }
+				// });
+
+				fetch(subscribeAction, {
+					method: "POST",
+					body: {
+						email: subscribeEmail.val(),
+					},
+				})
+					.then(() => {
+						subscribeEmail.val("");
+						subscribeEmail.blur();
+						toastr.success("Вы успешно подписались на рассылку новостей", "");
+					})
+					.catch(() => {
+						toastr.error("Попробуйте ещё раз.", "Ошибка!");
+					});
+			},
+		});
+	}
 })();
